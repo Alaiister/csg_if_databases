c4eff1328f2851d4d0ca601816d3a6816a7993e2
Alaiister <de.alaister@gmail.com>
2020-04-16 22:43:06 +0000

    Take1

--- public/PHP/opdracht_3.php
+++ public/PHP/opdracht_3.php
@@ -4,14 +4,22 @@ include('opdracht_begin.php');
 /****************************
 TYP HIERONDER JOUW PHPCODE
 ****************************/
-
+$a=2;
+$b=3;
+$c=round(sqrt(pow($a,2)+pow($b,2)),3);
+$modulo=fmod($b,$a);
 $waarde=9/10;
 $kwad=pow($waarde,2);
 $rec=1/$kwad;
-$afgerond=round($rec,7);
+$afgerond=floor(1000000*$rec)/1000000;
+
 
 echo "Het kwadraat van $waarde is $kwad.<br>
       Het omgekeerde daarvan is (afgerond:) $afgerond<br>";
+
+echo "Je houdt $modulo over bij $b / $a";
+
+
 
 /****************************
 EINDE VAN JOUW PHPCODE
